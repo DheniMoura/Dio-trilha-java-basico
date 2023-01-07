fc626b0b31cb071b4df03f3eeef49b22ee5466a4
DheniMoura <dhenifer@hotmail.com>
2023-01-07 14:27:44 -0300

    metodos

---
+++ tipos-variaveis/src/edu/dhenifer/metodos/usuario.java
@@ -0,0 +1,21 @@
+package edu.dhenifer.metodos;
+
+public class usuario {
+    
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(42);
+
+    }
+}
